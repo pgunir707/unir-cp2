cfd3a5ae746ccbd42f68d2751a8d3ce6d5de4c5d
pgunir707 <pedro.gomez707@comunidadunir.net>
2021-07-11 01:27:18 +0200

    master, workers and sdn

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -4,10 +4,16 @@ echo "Lanzando playbook de prerrequisitos"
 ansible-playbook -i hosts prerrequisitos.yaml
 
 echo "Lanzando playbook para instalar docker y kubernetes"
-ansible-playbook -i hosts -l master,worker k8s.yaml
+ansible-playbook -i hosts -l master,worker k8s-init.yaml
 
 echo "Lanzando playbook para montar el NFS Server"
 ansible-playbook -i hosts -l nfs nfs.yaml
 
 echo "Lanzando playbook para configurar el nodo master"
-ansible-playbook -i hosts -l master k8s-master-config.yaml
+ansible-playbook -i hosts -l master k8s-master.yaml
+
+echo "Lanzando playbook para instalar calico"
+ansible-playbook -i hosts -l master k8s-sdn.yaml
+
+echo "Lanzando playbook para configurar los workers"
+ansible-playbook -i hosts -l worker k8s-worker.yaml
