60d2ab2267c736b87e606c1892791b7831e91861
pgunir707 <pedro.gomez707@comunidadunir.net>
2021-07-10 21:21:30 +0200

    Add k8s master config

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 echo "Lanzando playbook de prerrequisitos"
-ansible-playbook -i hosts playbooks/prerrequisitos.yaml
+ansible-playbook -i hosts prerrequisitos.yaml
 
 echo "Lanzando playbook para instalar docker y kubernetes"
 ansible-playbook -i hosts -l master,worker k8s.yaml
 
 echo "Lanzando playbook para montar el NFS Server"
 ansible-playbook -i hosts -l nfs nfs.yaml
+
+echo "Lanzando playbook para configurar el nodo master"
+ansible-playbook -i hosts -l master k8s-master-config.yaml
