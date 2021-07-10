1cf288123c1e7c1b7b7602acb42522f33d8940b5
pgunir707 <pedro.gomez707@comunidadunir.net>
2021-07-10 19:14:30 +0200

    more

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -2,3 +2,9 @@
 
 echo "Lanzando playbook de prerrequisitos"
 ansible-playbook -i hosts playbooks/prerrequisitos.yaml
+
+echo "Lanzando playbook para instalar docker y kubernetes"
+ansible-playbook -i hosts -l master,worker k8s.yaml
+
+echo "Lanzando playbook para montar el NFS Server"
+ansible-playbook -i hosts -l nfs nfs.yaml
