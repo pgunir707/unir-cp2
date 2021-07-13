7a61ffc8d7b59873904bdfebb428978fefa9c9c5
pgunir707 <pedro.gomez707@comunidadunir.net>
2021-07-13 11:27:24 +0000

    change var ssh user

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -10,8 +10,8 @@ resource "azurerm_linux_virtual_machine" "myVM1" {
     disable_password_authentication = true
 
     admin_ssh_key {
-        username   = "adminUsername"
-        public_key = file("~/.ssh/id_rsa.pub")
+        username   = var.ssh_user
+        public_key = file(var.public_key_path)
     }
 
     os_disk {
